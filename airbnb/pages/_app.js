5efe951beb0118f16b0ba1b219d0e7bcff996095
Medos <73502818+Medos-dev@users.noreply.github.com>
2021-08-12 00:33:44 +0100

    Added header and banner

---
+++ airbnb/pages/_app.js
@@ -0,0 +1,10 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import 'tailwindcss/tailwind.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+  <Component {...pageProps} /></ChakraProvider>)
+}
+
+export default MyApp
---
+++ airbnb/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+      <Banner/>
+
+
+    </div>
+  )
+}
