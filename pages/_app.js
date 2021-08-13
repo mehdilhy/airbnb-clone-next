9bdf5e70952d1dc78970e5b1fe3735dd9f99ddcb
Medos <73502818+Medos-dev@users.noreply.github.com>
2021-08-13 18:31:21 +0100

    Added searching

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import 'tailwindcss/tailwind.css'
+import '../styles/global.css'
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from 'next/router';
+const progress = new ProgressBar({
+  size:8,
+  color:"black",
+  className:"z-50",
+  delay:100,
+});
+Router.events.on('routeChangeStart',progress.start);
+Router.events.on('routeChangeComplete',progress.finish);
+Router.events.on('routeChangeError',progress.finish);
+
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Header from "../components/Header";
+import Header from "../components/HeaderHome";
 import Banner from "../components/Banner";
 import SmallCard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
