6b3384bb5a385b5d74444585aabf1e86981d76c5
Medos <73502818+Medos-dev@users.noreply.github.com>
2021-08-15 21:56:10 +0100

    searchbar styling

--- components/SearchbarTab.js
+++ components/SearchbarTab.js
@@ -9,49 +9,51 @@ function SearchbarTab() {
     setValue(newValue);
   };
   return (
-    <div className=" hidden md:flex flex-row items-center md:border-1 rounded-full h-[66px] w-[800px] md-shadow-sm bg-white justify-between pb-[2px] p-2 font-bold">
-      <div className="flex flex-initial text-black text-[12px] font-weight:800 items-center">
+    <div className=" hidden md:flex flex-row  items-center md:border-1 rounded-full h-[68px] w-[800px] md-shadow-sm bg-white justify-between font-bold  ">
+      <div className="text-[12px] text-black h-[100%] flex flex-wrap content-center pl-7 border-r-2 rounded-full hover:bg-gray-200  transition transform duration-200 ease-out">
         <h1>Location</h1>
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
+          aria-expanded="false"
           placeholder="Where are you going?"
-          className="flex-grow pl-4 bg-transparent  outline-none placeholder-gray-500 "
+          className="flex-grow bg-transparent  outline-none placeholder-gray-500  cursor-pointer "
         />
+      
       </div>
-      <div className="text-black text-[12px]  font-weight:800 ">
+      <div className="text-[12px] text-black h-[100%] flex flex-wrap content-center pl-7 border-r-2 rounded-full hover:bg-gray-200  transition transform duration-200 ease-out">
         <h1>Check in</h1>
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Add dates"
-          className="flex-grow pl-4 bg-transparent  outline-none placeholder-gray-500 "
+          className="flex-grow bg-transparent  outline-none placeholder-gray-500  cursor-pointer "
         />
       </div>
-      <div className="text-black text-[12px]  font-weight:800">
+      <div className="text-[12px] text-black h-[100%] flex flex-wrap content-center pl-7 border-r-2 rounded-full hover:bg-gray-200  transition transform duration-200 ease-out">
         <h1>Check out</h1>
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Add dates"
-          className="flex-grow pl-4 bg-transparent  outline-none placeholder-gray-500 "
+          className="flex-grow bg-transparent  outline-none placeholder-gray-500  cursor-pointer "
         />
       </div>
-      <div className="text-black text-[12px]  font-weight:800">
+      <div className="flex flex-row hover:bg-gray-200 h-[100%] transition transform duration-200 ease-out rounded-full items-center">
+      <div className="text-[12px] text-black  flex flex-wrap content-center pl-7 ">
         <h1>Guests</h1>
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Add guests"
-          className="flex-grow pl-4 bg-transparent  outline-none placeholder-gray-500 placeholder-1 "
+          className="flex-grow bg-transparent  outline-none placeholder-gray-500  cursor-pointer "
         />
       </div>
-        <SearchIcon className="flex  h-[12] bg-[#FE375B] text-white rounded-full md:p-2 cursor-pointer justify-end" />
-      
+      <SearchIcon className="flex h-11  bg-[#FE375B] text-white rounded-full md:p-2 cursor-pointer justify-end " /></div>
     </div>
   );
 }
--- components/SearchbarTabExperience.js
+++ components/SearchbarTabExperience.js
@@ -1,35 +1,49 @@
 import { SearchIcon } from "@heroicons/react/solid";
 import React from "react";
 import { useState } from "react";
+import { DateRangePicker } from "react-date-range";
 
 function SearchbarTabExperience() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+  const resetInput = () => {
+    setSearchInput("");
+  };
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
   const [value, setValue] = useState(0);
   const [searchInput, setSearchInput] = useState("");
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
   return (
-    <div className=" hidden md:flex flex-row items-center md:border-1 rounded-full h-[66px] w-[800px] md-shadow-sm bg-white justify-between pb-[2px] p-2 font-bold">
-      <div className="flex flex-initial text-black text-[12px] font-weight:800 items-center">
+    <div className=" hidden md:flex flex-row  items-center md:border-1 rounded-full h-[68px] w-[800px] md-shadow-sm bg-white justify-between font-bold  ">
+      <div className="text-[12px] text-black h-[100%] flex flex-wrap content-center pl-7 border-r-2 rounded-full hover:bg-gray-200  transition transform duration-200 ease-out">
         <h1>Location</h1>
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Where are you going?"
-          className="flex-grow pl-4 bg-transparent  outline-none placeholder-gray-500 "
+          className="flex-grow bg-transparent  outline-none placeholder-gray-500  cursor-pointer "
         />
       </div>
-      <div className="text-black text-[12px]  font-weight:800 ">
+      <div className="text-[12px] text-black h-[100%] flex flex-wrap content-center pl-7 border-r-2 rounded-full hover:bg-gray-200  transition transform duration-200 ease-out">
         <h1>Date</h1>
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
-          placeholder="Add when you want to go"
-          className="flex-grow pl-4 bg-transparent  outline-none placeholder-gray-500 "
-        />
-      </div>
+          placeholder="Where are you going?"
+          className="flex-grow bg-transparent  outline-none placeholder-gray-500  cursor-pointer "
+        />      </div>
       <div>
         <SearchIcon className="flex h-[12] bg-[#FE375B] text-white rounded-full md:p-2 cursor-pointer justify-center" />
       </div>
